df4f99a0479e351cb14b5d306c2194f161fc66e9
Astijus Karvauskas <astijusk7@gmail.com>
2025-09-17 18:45:02 +0100

    updated to instantiate Renderer and Physics handler as well as the Ball class

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
+from Renderer import Renderer
+from Ball import Ball
+from Physics import Physics
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+renderer = Renderer(screen)
+physics = Physics(dt)
+
+ball = Ball(screen.get_width() / 2, screen.get_height() / 2, 0, 0)
+physics.set_ball_velocity(ball, 100, 0)
 
 while running:
     # poll for events
@@ -18,10 +29,16 @@ while running:
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
+    renderer.draw_ball(ball)
+    physics.update_ball(ball, dt)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for fps independent physics
+    dt = clock.tick(60) / 1000
+
+
 
 pygame.quit()
