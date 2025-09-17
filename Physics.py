c44085164083756a80e3cd942fe7045e3c2ad469
Astijus Karvauskas <astijusk7@gmail.com>
2025-09-17 23:28:29 +0100

    added friction / colliding with walls

--- Physics.py
+++ Physics.py
@@ -1,3 +1,7 @@
+import constants as c
+from constants import FRICTION, MIN_SPEED_FOR_STOP
+
+
 class Physics:
     def __init__(self, dt):
         self.dt = dt
@@ -11,3 +15,16 @@ class Physics:
     def update_ball(ball, dt):
         ball.x += ball.xv * dt
         ball.y += ball.yv * dt
+
+        ball.xv *= FRICTION
+        ball.yv *= FRICTION
+
+        if abs(ball.xv) <= MIN_SPEED_FOR_STOP: ball.xv = 0
+        if abs(ball.yv) <= MIN_SPEED_FOR_STOP: ball.yv = 0
+
+    @staticmethod
+    def check_collision(ball):
+        if ball.x < (0 + ball.radius) or ball.x > (c.SCREEN_WIDTH - ball.radius):
+            ball.xv = ball.xv * -1
+        if ball.y < (0 + ball.radius) or ball.y > (c.SCREEN_HEIGHT - ball.radius):
+            ball.yv = ball.yv * -1
