e8e71d8da99f101e59b257860b9d797d9559f259
Astijus Karvauskas <astijusk7@gmail.com>
2025-09-19 00:55:30 +0100

    Redirected input handling to InputHandler class

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
-from Renderer import Renderer
-from Ball import Ball
-from Physics import Physics
+import constants as c
+
+from inputHandler import InputHandler
+from renderer import Renderer
+from ball import Ball
+from physicsHandler import Physics
+from action import *
+
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
-running = True
 dt = 0
 
 renderer = Renderer(screen)
-physics = Physics(dt)
+physics = Physics()
+inputHandler = InputHandler()
 
 ball = Ball(screen.get_width() / 2, screen.get_height() / 2, 0, 0)
-physics.set_ball_velocity(ball, 100, 0)
+physics.set_ball_velocity(ball, 0, 1000)
 
-while running:
+while c.RUNNING:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        action = inputHandler.handle_input(event)
+
+        if action and action.type == ActionType.SHOOT:
+            # TODO: Implement shoot ball mechanic
+            m_x = action.payload[0]
+            m_y = action.payload[1]
+
+
+            pass
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
     renderer.draw_ball(ball)
-    physics.update_ball(ball, dt)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -38,7 +49,7 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for fps independent physics
     dt = clock.tick(60) / 1000
-
-
+    physics.update_ball(ball, dt)
+    physics.check_collision(ball)
 
 pygame.quit()
